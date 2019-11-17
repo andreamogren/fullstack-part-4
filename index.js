44a94e65972456c8bb3403d899be9d43edb19dbd
Andrea <andrea.mogren@comprend.com>
2019-11-17 19:15:19 +0100

    Exercise 4.1 done

--- index.js
+++ index.js
@@ -1,121 +1,46 @@
 require('dotenv').config()
+const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/person')
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+const mongoose = require('mongoose')
 
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError' && error.errors.name.kind === 'unique') {
-    return response.status(400).send({ error: 'name must be unique' })
-  }
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-  next(error)
-}
+const Blog = mongoose.model('Blog', blogSchema)
 
-let persons = [
-  {
-    'name': 'Arto Hellas',
-    'number': '040-123456',
-    'id': 1
-  },
-  {
-    'name': 'Ada Lovelace',
-    'number': '39-44-5323523',
-    'id': 2
-  },
-  {
-    'name': 'Dan Abramov',
-    'number': '12-43-234345',
-    'id': 3
-  },
-  {
-    'name': 'Mary Poppendieck',
-    'number': '39-23-6423122',
-    'id': 4
-  }
-]
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
-app.use(express.static('build'))
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(people => {
-    response.json(people.map(person => person.toJSON()))
-  })
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person.toJSON())
-  })
-    .catch(error => {
-      console.log(error)
-      response.status(404).end()
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
     })
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  }
-
-  /* const addedName = body.name
-    const nameExists = persons.find(person =>  person.name === addedName)
-    if (nameExists) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    } */
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-
-  person
+  blog
     .save()
-    .then(savedPerson => {
-      response.json(savedPerson.toJSON())
+    .then(result => {
+      response.status(201).json(result)
     })
-    .catch(error => next(error))
 })
 
-app.get('/api/info', (request, response) => {
-  const requestDate = new Date()
-  response.send(`
-                    <p>Phonebook has info for ${persons.length} people</p>
-                    <p>${requestDate}</p>
-                `)
+const PORT = 3003
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
 })
-
-app.use(errorHandler)
-
-const port = process.env.PORT
-app.listen(port)
-console.log(`Server running on port ${port}`)
