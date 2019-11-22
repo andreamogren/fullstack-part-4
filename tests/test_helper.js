b010610e4ce300c974e02d3bc54e114241933f5e
Andrea <andrea.mogren@comprend.com>
2019-11-22 20:57:05 +0100

    async/await excersises wip

---
+++ tests/test_helper.js
@@ -0,0 +1,29 @@
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    important: true
+  }
+]
+
+const nonExisitingId = async () => {
+  const note = new Note({ content: 'willremovethisson' })
+  await note.save()
+  await note.remove()
+
+  return note._id.toString()
+}
+
+const notesInDb = async () => {
+  const notes = await Note.find({})
+  return notes.map(note => note.toJSON())
+}
+
+module.exports = {
+  initialNotes, nonExisitingId, notesInDb
+}
