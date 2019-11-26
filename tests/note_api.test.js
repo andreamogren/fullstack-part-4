ef26e47678800f97e36531a465c7790ab30fd735
Andrea <andrea.mogren@comprend.com>
2019-11-26 21:07:55 +0100

    started with tutorial exercises in part 4c

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,6 +5,7 @@ const app = require('../app')
 const api = supertest(app)
 
 const Note = require('../models/note')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Note.deleteMany({})
@@ -123,6 +124,36 @@ describe('deletion of a note', () => {
     const contents = notesAtEnd.map(r => r.content)
 
     expect(contents).not.toContain(noteToDelete.content)
+  })
+})
+
+describe('when there is one user in db initially', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const user = new User({ username: 'root', password: 'secret' })
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
   })
 })
 
