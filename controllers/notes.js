1f9a07b0a6482f4071f8b06271dd0497247ad461
Andrea <andrea.mogren@comprend.com>
2019-11-30 09:31:14 +0100

    using tokens to add notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response, next) => {
   try {
@@ -28,19 +37,23 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  console.log('userId', body.userId)
-
-  const user = await User.findById(body.userId)
-  console.log('user', user)
-
-  const note = new Note({
-    content: body.content,
-    important: body.important === undefined ? false : body.important,
-    date: new Date(),
-    user: user._id
-  })
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const note = new Note({
+      content: body.content,
+      important: body.important === undefined ? false : body.important,
+      date: new Date(),
+      user: user._id
+    })
+
     const savedNote = await note.save()
     user.notes = user.notes.concat(savedNote._id)
     await user.save()
